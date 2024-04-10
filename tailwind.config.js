9bb3ee456d6d86bf492293c1976a4a1fd5cc049c
Spandan Barve <spandan567@gmail.com>
2024-04-10 16:24:35 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        mobile: { max: "780px" },
+        widescreen: { min: "780px" },
+      },
+      colors: {
+        primary: "#022041",
+        secondary: "#0765d0",
+        background: "#ffffff",
+        foreground: "#f6fafd",
+        front: "#070724",
+        back: "#ffffff",
+      },
+      borderRadius: {
+        inherit: "inherit",
+      },
+      transitionDuration: {
+        inherit: "inherit",
+      },
+      fontFamily: {
+        poppins: '"Poppins", sans-serif',
+        raleway: '"Raleway", sans-serif',
+      },
+      zIndex: {
+        1: 1,
+      },
+    },
+  },
+  plugins: [],
+};
