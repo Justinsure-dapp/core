fc970c2b576c0aee023f354f13f40ddf0cbfa2ce
Spandan Barve <spandan567@gmail.com>
2024-04-11 15:26:38 +0530

    staking stats and surity info on statisticsidebar

---
+++ src/common/StatisticsSidebar/index.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import { linearMapColor, rgbToHex } from "../../utils";
+import Icon, { IconType } from "../Icon";
+import { Link } from "react-router-dom";
+import useIdleScrollbar from "../../hooks/useIdleScrollbar";
+import StakingStats from "./components/StakingStats";
+import SurityInfo from "./components/SurityInfo";
+
+export default function StatisticsSidebar() {
+  return (
+    <section className="flex relative flex-col border-l border-border max-w-[20vw] h-screen">
+      <div className="p-6 flex flex-col gap-y-2">
+        <h1 className="text-mute text-base font-bold">SureCoin Balance</h1>
+        <div>
+          <h2 className="text-xs -mb-1">Wallet</h2>
+          <p className="font-mono text-primary text-2xl font-medium">103.00</p>
+        </div>
+        <div>
+          <h2 className="text-xs -mb-1">Pending</h2>
+          <p className="font-mono text-primary text-2xl font-medium">84.20</p>
+        </div>
+      </div>
+
+      <StakingStats />
+
+      <figure role="separator" className="flex-1" />
+
+      <SurityInfo />
+    </section>
+  );
+}
