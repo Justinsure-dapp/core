556834000f241f4e1e2a4f97ce42c84f8e0d1f04
Riya Jain <riya1jain567@gmail.com>
2024-05-02 13:31:24 +0530

    Modified your policies desgin

--- src/pages/AccountPage/components/YourPolicies.tsx
+++ src/pages/AccountPage/components/YourPolicies.tsx
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import { closestTimeUnit } from "../../../utils";
 
 export default function YourPolicies() {
+  const [viewMore, setViewMore] = useState(false);
+
   return (
     <div className="py-4 flex flex-col gap-y-8">
-      <div className="flex justify-between items-start">
+      <div className="flex justify-between items-end">
         <h1 className="text-xl font-bold tracking-wide">Your Policies</h1>
         <div className="flex flex-col gap-y-2 items-end">
           <p className="font-mono">SureCoin: 103.00</p>
@@ -12,71 +15,82 @@ export default function YourPolicies() {
           </button>
         </div>
       </div>
-      <div className="flex gap-y-8 flex-col">
-        {policies.map((policy, key) => (
-          <div
-            className="bg-secondary/5 rounded-lg flex flex-col p-4 border border-primary/25"
-            key={key}
-          >
-            <div className="flex gap-x-4">
-              <img
-                src={policy.logoUrl}
-                className="w-[5vw] rounded-full h-max"
-              />
-              <div className="flex flex-col">
-                <h1 className="text-xl font-bold tracking-wide">
-                  {policy.name}
-                </h1>
-                <p className="text-sm mt-1 font-light text-front/90">
-                  {policy.description}
-                </p>
-                <div className="mt-2 self-end text-end w-full">
-                  {policy.boughtAt + policy.duration - Date.now() < 0 ? (
-                    <>
-                      <p>
-                        {" "}
-                        Status:{" "}
-                        {policy.claimed ? (
-                          <span className="text-green-500">Claimed</span>
-                        ) : (
-                          <span className="text-red-500">Expired</span>
-                        )}
-                      </p>
+      <div className="flex gap-y-8 flex-col p-6 rounded-lg bg-primary/5">
+        {policies.map(
+          (policy, key) =>
+            (viewMore || key < 2) && (
+              <div
+                className="bg-background rounded-lg flex flex-col p-4 border border-primary/25"
+                key={key}
+              >
+                <div className="flex gap-x-4">
+                  <img
+                    src={policy.logoUrl}
+                    className="w-[5vw] rounded-full h-max"
+                  />
+                  <div className="flex flex-col">
+                    <h1 className="text-xl font-bold tracking-wide">
+                      {policy.name}
+                    </h1>
+                    <p className="text-sm mt-1 font-light text-front/90">
+                      {policy.description}
+                    </p>
+                    <div className="mt-2 self-end text-end w-full">
+                      {policy.boughtAt + policy.duration - Date.now() < 0 ? (
+                        <>
+                          <p>
+                            {" "}
+                            Status:{" "}
+                            {policy.claimed ? (
+                              <span className="text-green-500">Claimed</span>
+                            ) : (
+                              <span className="text-red-500">Expired</span>
+                            )}
+                          </p>
 
-                      <p className="">
-                        Policy expired{" "}
-                        <span className="text-red-500">
-                          {closestTimeUnit(
-                            Date.now() - policy.duration - policy.boughtAt
-                          )}{" "}
-                        </span>
-                        ago{" "}
-                      </p>
-                    </>
-                  ) : (
-                    <div className="flex justify-between">
-                      <button className="mt-2 bg-primary px-4 py-2 text-back font-bold rounded-lg text-sm">
-                        Request Claim
-                      </button>
-                      <div className="flex flex-col">
-                        <p className="text-sm">
-                          Status:{" "}
-                          <span className="text-orange-500">Ongoing</span>
-                        </p>
-                        <p className="text-sm">
-                          TIme Left:{" "}
-                          {closestTimeUnit(
-                            policy.boughtAt + policy.duration - Date.now()
-                          )}{" "}
-                        </p>
-                      </div>
+                          <p className="">
+                            Policy expired{" "}
+                            <span className="text-red-500">
+                              {closestTimeUnit(
+                                Date.now() - policy.duration - policy.boughtAt
+                              )}{" "}
+                            </span>
+                            ago{" "}
+                          </p>
+                        </>
+                      ) : (
+                        <div className="flex justify-between">
+                          <button className="mt-2 bg-primary px-4 py-2 text-back font-bold rounded-lg text-sm">
+                            Request Claim
+                          </button>
+                          <div className="flex flex-col">
+                            <p className="text-sm">
+                              Status:{" "}
+                              <span className="text-orange-500">Ongoing</span>
+                            </p>
+                            <p className="text-sm">
+                              Time Left:{" "}
+                              {closestTimeUnit(
+                                policy.boughtAt + policy.duration - Date.now()
+                              )}{" "}
+                            </p>
+                          </div>
+                        </div>
+                      )}
                     </div>
-                  )}
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        ))}
+            )
+        )}
+        {policies.length > 2 &&
+          <button
+            className="bg-primary w-max px-4 py-2 self-end text-back font-bold rounded-lg -translate-y-2"
+            onClick={() => setViewMore(!viewMore)}
+          >
+            {viewMore ? "View Less" : "View More"}{" "}
+          </button>
+        }
       </div>
     </div>
   );
