59a5838d7b3ba50820b82491125754d693f8b05c
Riya Jain <riya1jain567@gmail.com>
2024-05-02 14:43:59 +0530

    Added your stakes component in Account

--- src/pages/AccountPage/index.tsx
+++ src/pages/AccountPage/index.tsx
@@ -1,11 +1,13 @@
 import DocTitle from "../../common/DocTitle";
 import YourPolicies from "./components/YourPolicies";
+import YourStakes from "./components/YourStakes";
 
 export default function AccountPage() {
   return (
     <article className="relative p-page">
       <DocTitle title="My Account" />
       <YourPolicies />
+      <YourStakes />
     </article>
   );
 }
