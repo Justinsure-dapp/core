f47ed850829043a3bc549ab4a5ec5aa7a10016e0
Riya Jain <riya1jain567@gmail.com>
2024-05-04 14:42:45 +0530

    Dasboard policies display

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,34 +1,63 @@
+import DocTitle from "../../common/DocTitle";
+
 export default function DashboardPage() {
   return (
     <section className="p-page py-4">
+        <DocTitle title="Dashboard" />
       <h1 className="text-xl font-bold">Your Policies</h1>
       <div className="flex flex-col gap-y-8 mt-4">
         {policies.map((policy, i) => (
-          <div className="flex flex-col gap-y-4 bg-primary/5 p-4 rounded-lg border border-secondary/20">
+          <div className="flex flex-col gap-y-4 bg-primary/5 p-4 rounded-lg border border-secondary/20 relative">
             <div className="flex flex-col gap-y-1">
               <h1 className="text-xl font-semibold">{policy.name}</h1>
               <div className="text-front/80 text-sm">{policy.description}</div>
             </div>
             <div className="flex gap-x-4 flex-wrap gap-y-4">
-              <div className="bg-background hover:bg-front hover:bg-opacity-[1%] duration-300 ease-in-out border border-front/20 w-max flex px-4 py-3 rounded-xl gap-x-16 justify-between items-center">
+              <div className="bg-background hover:bg-front hover:bg-opacity-[1%] duration-300 ease-in-out border border-front/20 w-max flex px-4 py-3 rounded-xl gap-x-8 justify-between items-center">
                 <div className="flex flex-col">
                   <h1 className="text-xl font-bold font-mono">1242</h1>
-                  <p className="text-front/80 flex items-center text-sm">Policy Holders &#160; <span className="text-green-500">+21.4%</span></p>
+                  <p className="text-front/80 flex items-center text-sm">
+                    Policy Holders &#160;{" "}
+                    <span className="text-green-500">+21.4%</span>
+                  </p>
                 </div>
                 <div className="p-2 bg-green-500/20 rounded-xl">
                   <img src="https://img.icons8.com/ios-filled/32/40C057/bullish.png" />
                 </div>
               </div>
-              <div className="bg-background hover:bg-front hover:bg-opacity-[1%] duration-300 ease-in-out border border-front/20 w-max flex px-4 py-3 rounded-xl gap-x-16 justify-between items-center">
+              <div className="bg-background hover:bg-slate-400 hover:bg-opacity-[1%] duration-300 ease-in-out border border-front/20 w-max flex px-4 py-3 rounded-xl gap-x-8 justify-between items-center">
                 <div className="flex flex-col">
                   <h1 className="text-xl font-bold font-mono">345</h1>
-                  <p className="text-front/80 text-sm">Stake Holders &#160; <span className="text-red-500">-12.43%</span></p>
+                  <p className="text-front/80 text-sm">
+                    Stake Holders &#160;{" "}
+                    <span className="text-red-500">-12.43%</span>
+                  </p>
                 </div>
                 <div className="p-2 bg-red-500/20 rounded-xl">
                   <img src="https://img.icons8.com/ios-filled/32/FA5252/bearish.png" />
                 </div>
               </div>
-             
+              <div className="bg-background hover:bg-slate-400 hover:bg-opacity-[1%] duration-300 ease-in-out border border-front/20 w-max flex px-4 py-3 rounded-xl gap-x-8 justify-between items-center">
+                <div className="flex flex-col">
+                  <h1 className="text-xl font-bold font-mono">345</h1>
+                  <p className="text-front/80 text-sm">
+                    Money in Pool &#160;{" "}
+                    {/* <span className="text-red-500">-12.43%</span> */}
+                  </p>
+                </div>
+                <div className="p-2 bg-front/20 rounded-xl">
+                  <img src="https://img.icons8.com/pulsar-color/32/money-bag.png" />
+                </div>
+              </div>
+            </div>
+            <button className="absolute bottom-2 right-3 underline">
+              View More
+            </button>
+            <div className="flex flex-col">
+                <button className="bg-front/20 w-max py-2 px-3 rounded-lg">
+                    Recent Activity
+                </button>
+                <div></div>
             </div>
           </div>
         ))}
