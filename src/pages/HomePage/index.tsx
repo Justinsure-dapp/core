8668c43ea3169e5e85402765888d3b241aabab5e
Riya Jain <riya1jain567@gmail.com>
2024-05-08 01:31:49 +0530

    Add telegram banner in home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -81,6 +81,35 @@ export default function HomePage() {
             </article>
           </div>
         </div>
+        <Link
+          to="https://t.me/surity_bot"
+          target={`_newABC`}
+          className="flex items-start justify-center mt-8 w-full"
+        >
+          <div className="relative group">
+            <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-100 group-hover:duration-200 animate-tilt"></div>
+            <button className="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+              <span className="flex items-center space-x-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6 text-pink-600"
+                >
+                  <path d="M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7l-5.45-1.7c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z" />
+                </svg>
+
+                <span className="pr-6 text-gray-100">
+                  We are live on Telegram
+                </span>
+              </span>
+              <span className="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">
+                Click here to try it &rarr;
+              </span>
+            </button>
+          </div>
+        </Link>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+        animation: {
+          tilt: 'tilt 10s infinite linear',
+        },
+        keyframes: {
+          tilt: {
+            '0%, 50%, 100%': {
+              transform: 'rotate(0deg)',
+            },
+            '25%': {
+              transform: 'rotate(0.5deg)',
+            },
+            '75%': {
+              transform: 'rotate(-0.5deg)',
+            },
+          },
+      },
       screens: {
         mobile: { max: "780px" },
         widescreen: { min: "780px" },
