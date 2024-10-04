8480c26ec87de8d13a40b580fc38ca0228822e75
Riya Jain <riya1jain567@gmail.com>
2024-10-04 13:04:11 +0000

    Improved ui for stake modal

---
+++ app/pages/PolicyPage/components/Header1.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header1() {
+    return (
+        <div className='w-full flex'>
+            <div className='w-2/3 bg-gradient-to-b from-primary/50 to-front/30 h-[20vh] rounded-md'>
+            </div>
+        </div>
+    )
+}
