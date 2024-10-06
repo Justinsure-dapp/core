98c3ad183e6f34ed20f05f345181add0a15feddb
Styles <kartik100100@gmail.com>
2024-10-06 16:35:16 +0530

    revert policy page

--- app/pages/PolicyPage/components/Header1.tsx
+++ app/pages/PolicyPage/components/Header1.tsx
@@ -13,8 +13,8 @@ export default function Header1({ policy }: { policy: Policy }) {
   const modal = useModal();
 
     return (
-        <div className="flex flex-col gap-1 bg-foreground/50 px-6 py-4 relative">
-            <div className='w-full  gap-4 rounded-md flex '>
+        <div className="flex flex-col gap-1">
+            <div className='w-full bg-foreground/50 gap-4 rounded-md flex px-6 py-4'>
                 <div className="max-w-[10rem] w-full">
                     <img
                         src={policy.image}
@@ -22,14 +22,14 @@ export default function Header1({ policy }: { policy: Policy }) {
                         alt="Policy"
                     />
                 </div>
-                <div className='flex flex-col w-full justify-between'>
+                <div className='flex flex-col w-full'>
                     <h1 className='text-3xl font-bold tracking-wider truncate whitespace-nowrap'>{policy.name}</h1>
-                    <p className='border border-mute w-max px-3 mt-2 rounded-xl text-sm h-max absolute top-4 right-4 '>Category: {policy.category}</p>
-                    <p className='text-sm text-front/60 mt-1'>
-                        {`${policy.description.slice(0, 200)}${policy.description.length > 200 ? '...' : ''}`}
+                    <p>Category: {policy.category}</p>
+                    <p className='text-sm text-front/60'>
+                        {`${policy.description.slice(0, 150)}${policy.description.length > 150 ? '...' : ''}`}
                     </p>
 
-                    <div className='flex gap-x-4 mt-5'>
+                    <div className='flex gap-x-4 mt-4'>
                         <img src={user?.marketer?.image} className='w-12 shadow-sm rounded-full aspect-square object-cover' />
                         <div className='text-sm flex flex-col gap-y-1'>
                             <p>Marketer: {user?.marketer?.name}</p>
@@ -43,7 +43,7 @@ export default function Header1({ policy }: { policy: Policy }) {
             </div>
 
 
-            <div className='absolute bottom-4 right-4 flex gap-x-2'>
+            <div className='flex gap-x-4 self-end mt-3'>
                 <Link to={`/buy-policy/${policy.address}`} className='bg-front hover:bg-front/80 transition-all text-back px-4 rounded-2xl border border-border py-1 font-semibold'>Buy Policy</Link>
                 <button className='bg-primary border border-border hover:bg-primary/80 transition-all text-front px-6 rounded-2xl py-1 font-semibold' onClick={() => {
                     modal.show(<StakeModal policy={policy} />);
