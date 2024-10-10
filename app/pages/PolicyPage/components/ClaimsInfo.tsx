92602ce46d5a23f20a910825e8a2bfefe2f1c8fa
Styles <kartik100100@gmail.com>
2024-10-10 18:25:49 +0530

    fixed policyPage UI responsiveness

--- app/pages/PolicyPage/components/ClaimsInfo.tsx
+++ app/pages/PolicyPage/components/ClaimsInfo.tsx
@@ -3,15 +3,10 @@ import { Policy } from "../../../types";
 import { closestTimeUnit } from "../../../utils";
 import contractDefinitions from "../../../contracts";
 import { isAddress } from "viem";
-import PolicyCard from "../../DashboardPage/components/PolicyCard";
 import useUsdjHook from "../../../hooks/useUsdj";
 
-export default function ClaimInfo(props: { policy: Policy }) {
-  const { policy } = props;
-
+export default function ClaimInfo({ policy }: { policy: Policy }) {
   const usdj = useUsdjHook();
-  const { divideByDecimals } = useUsdjHook();
-
   if (!isAddress(policy.address)) return <></>;
 
   const { data: totalStake } = useReadContract({
@@ -22,14 +17,14 @@ export default function ClaimInfo(props: { policy: Policy }) {
 
   return (
     <div>
-      <div className="mt-4 flex w-full gap-x-4 gap-y-7 text-front/60 mobile:gap-x-4">
-        <div className="flex w-1/3 flex-col gap-y-2 text-sm">
-          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5 mobile:w-[30%]">
+      <div className="mt-4 flex mobile:flex-col w-full gap-x-4 gap-y-7 text-front/60 mobile:gap-x-4">
+        <div className="flex w-1/3 mobile:w-full flex-col gap-y-2 text-sm">
+          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5">
             <h1 className="tracking-wide">Policy Holders: </h1>
             <p className="text-front/80">{policy.holders.length}</p>
           </div>
 
-          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5 mobile:w-[30%]">
+          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5">
             <h1 className="tracking-wide">Total Stake:</h1>
             <p className="text-front/80">
               {" "}
@@ -37,13 +32,13 @@ export default function ClaimInfo(props: { policy: Policy }) {
             </p>
           </div>
 
-          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5 mobile:w-[30%]">
+          <div className="flex items-center justify-center gap-x-2 rounded-md border border-border bg-background py-1 text-center duration-200 ease-in-out hover:bg-front/5">
             <h1 className="tracking-wide">Total Claims:</h1>
             <p className="text-front/80">{policy.claims.length} </p>
           </div>
         </div>
-        <div className="flex w-2/3 gap-x-4">
-          <div className="flex w-1/2 flex-col justify-center rounded-md border border-border bg-background px-3 duration-200 ease-in-out mobile:w-full">
+        <div className="flex w-2/3 mobile:w-full gap-x-4">
+          <div className="flex w-1/2 flex-col justify-center rounded-md border border-border bg-background px-3 duration-200 p-2 ease-in-out mobile:w-full">
             {" "}
             <h1 className="border-b-2 border-foreground tracking-wide">
               Duration for the policy
@@ -61,7 +56,7 @@ export default function ClaimInfo(props: { policy: Policy }) {
               </span>
             </div>
           </div>
-          <div className="flex w-1/2 flex-col justify-center rounded-md border border-border bg-background px-3 duration-200 ease-in-out mobile:w-full">
+          <div className="flex w-1/2 flex-col justify-center rounded-md border border-border bg-background px-3 duration-200 p-2 ease-in-out mobile:w-full">
             {" "}
             <h1 className="border-b-2 border-foreground tracking-wide">
               Limit of the claim
--- app/pages/PolicyPage/components/Header.tsx
+++ app/pages/PolicyPage/components/Header.tsx
@@ -13,44 +13,72 @@ export default function Header({ policy }: { policy: Policy }) {
   const modal = useModal();
 
   return (
-    <div className="relative flex flex-col justify-between gap-1 bg-foreground/50 px-6 py-4">
-      <div className="flex w-full gap-4 rounded-md">
-        <div className="w-full max-w-[10rem]">
-          <img
-            src={policy.image}
-            className="aspect-square rounded-md object-cover"
-            alt="Policy"
-          />
-        </div>
-        <div className="flex w-full flex-col justify-between">
-          <p className="absolute right-4 top-4 mt-2 h-max w-max rounded-xl border border-mute px-3 text-sm">
-            Category: {policy.category}
-          </p>
-          <div className="flex flex-col">
-            <h1 className="truncate whitespace-nowrap text-3xl font-bold tracking-wider">
-              {policy.name}
-            </h1>
-            <p className="mt-1 text-sm text-front/60">
-              {`${policy.description.slice(0, 200)}${policy.description.length > 200 ? "..." : ""}`}
+    <div className="w-full flex flex-col items-end gap-2">
+      <div className="relative flex flex-col justify-between gap-1 bg-foreground/50 px-6 py-4">
+        <div className="flex w-full gap-4 rounded-md">
+          <div className="w-full max-w-[10rem] flex flex-col items-center gap-4">
+            <img
+              src={policy.image}
+              className="aspect-square rounded-md object-cover"
+              alt="Policy"
+            />
+            <p className="h-max widescreen:hidden w-max rounded-xl border border-mute px-3 text-sm">
+              Category: {policy.category}
             </p>
           </div>
-
-          <div className="mt-2 flex gap-x-4">
-            <img
-              src={user?.marketer?.image}
-              className="aspect-square w-12 rounded-full object-cover shadow-sm"
-            />
-            <div className="flex flex-col gap-y-1 text-sm">
-              <p>Marketer: {user?.marketer?.name}</p>
-              <p className="text-xs">
-                {user && formatEvmAddress(user?.address)}
+          <div className="flex w-full flex-col justify-between">
+            <div className="flex flex-col gap-1">
+              <div className="w-full flex flex-col gap-2 widescreen:flex-row justify-between">
+                <h1 className="truncate whitespace-nowrap text-3xl font-bold tracking-wider">
+                  {policy.name}
+                </h1>
+                <p className="h-max w-max rounded-xl border border-mute px-3 text-sm">
+                  Category: {policy.category}
+                </p>
+              </div>
+              <p className="mt-1 text-sm text-front/60">
+                {`${policy.description.slice(0, 200)}${policy.description.length > 200 ? "..." : ""}`}
               </p>
+            </div>
+
+            <div className="flex items-center w-full justify-between">
+              <div className="mt-2 flex gap-x-4">
+                <img
+                  src={user?.marketer?.image}
+                  className="aspect-square w-12 rounded-full object-cover shadow-sm"
+                />
+                <div className="flex flex-col gap-y-1 text-sm">
+                  <p>Marketer: {user?.marketer?.name}</p>
+                  <p className="text-xs">
+                    {user && formatEvmAddress(user?.address)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="hidden md:flex gap-x-2">
+                <Link
+                  to={`/buy-policy/${policy.address}`}
+                  className="rounded-2xl border border-border bg-front px-4 py-1 font-semibold text-back transition-all hover:bg-front/80"
+                >
+                  Buy Policy
+                </Link>
+                <button
+                  className="rounded-2xl border border-border bg-primary px-6 py-1 font-semibold text-front transition-all hover:bg-primary/80"
+                  onClick={() => {
+                    modal.show(<StakeModal policy={policy} />);
+                  }}
+                >
+                  Stake
+                </button>
+              </div>
             </div>
           </div>
         </div>
+
+
       </div>
 
-      <div className="absolute bottom-4 right-4 flex gap-x-2">
+      <div className="md:hidden flex gap-x-2">
         <Link
           to={`/buy-policy/${policy.address}`}
           className="rounded-2xl border border-border bg-front px-4 py-1 font-semibold text-back transition-all hover:bg-front/80"
